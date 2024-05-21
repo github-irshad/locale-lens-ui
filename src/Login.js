27dc9909718616697e284aaf6c6f588d5debad8e
github-irshad <irshadaj08@gmail.com>
2024-05-21 21:26:11 +0530

    axios post for registration

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React,{ Fragment } from "react";
 function Login(){
 return (
-    <div>
-        Login
-    </div>
+    <Fragment>
+      <div>Login</div>
+      <label>Username</label>
+      <input type="text" placeholder="Username" id="txtUsername"/><br></br>
+      <label>Password</label>
+      <input type="password" placeholder="Confirm Password" id="cnfmPassword"/><br></br>
+      <button>Login</button>
+    </Fragment>
 )
 }
 
